cb8d5ee430d6975c80face7bee76523f2fbc8e2c
Víctor Enrique Montes Tovar <vmontestovar@nautilus19.ad.danielcastelao.org>
2019-10-08 10:38:26 +0200

    inicio

---
+++ extra1_2/Extra1_2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extra1_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author vmontestovar
+ */
+public class Extra1_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        System.out.println("Teclee el radio del triangulo");
+        float radio=teclado.nextFloat();
+        float area=(float)(2*Math.PI*radio);
+        System.out.println("El area del triangulo es "+area);
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
